167a8ec04628e4cdb5eb1ceadcc68734542863f8
i311648 <asya.chakarova@sap.com>
2018-11-05 18:23:26 +0200

    Start over

--- src/bg/sofia/uni/fmi/mjt/git/Repository.java
+++ src/bg/sofia/uni/fmi/mjt/git/Repository.java
@@ -1,103 +1,91 @@
 package bg.sofia.uni.fmi.mjt.git;
 
+import java.util.Collections;
 import java.util.LinkedHashSet;
 
 public class Repository {
 
-    private Branch branch = null;
-    private Result result;
-    private LinkedHashSet<String> storage = null;
-    private Staging st;
-    private int lastCommitChanges = 0;
-    private boolean duplication;
-    private String resultMessage = null;;
-    private int filesChangedAdd = 0;
-    private int filesChangedRm = 0;
-    private Commit commit;
+    private LinkedHashSet<String> staging;
+    private Branch branch;
+    private String resultMessage;
+    private int filesChangedAdd; //TODO - change to filesChanged : both from add and remove
 
     public Repository() {
-        storage = new LinkedHashSet<>();
-        setBranch(new Branch());
-        result = new Result();
-        commit = new Commit();
+        staging = new LinkedHashSet<>();
+        branch = new Branch();
     }
 
     public Result add(String... files) {
 
-        // st = new Staging(storage, result, files);
-        // st.stageAdd();
+        filesChangedAdd = 0;
+        boolean duplication = false;
+        Result result = new Result();
+
         for (String file : files) {
-            if (storage.contains(file)) {
+            if (staging.contains(file)) {
                 resultMessage = (file + "already exists");
                 duplication = true;
                 break;
-            } else {
-                storage.add(file);
-                filesChangedAdd++;
             }
         }
-        result.setDuplicationStatus(duplication);
-        if (duplication) {
-            result.setMessage(resultMessage);
-        } else {
-            lastCommitChanges = filesChangedAdd;
-            result.setMessage(buildMessage("add-t", files));
+        if (!duplication) {
+            Collections.addAll(staging, files); // TODO - check if working; have
+                                                // to add all files to staging
+            resultMessage = buildMessage("add-t", files);
+            filesChangedAdd++;
         }
-
-        return result;
-    }
-
-    public Result remove(String... files) {
-        // st = new Staging(storage, result, files);
-        // st.stageRm();
-        for (String file : files) {
-            if (storage.contains(file)) {
-                duplication = true; // there is a match
-                filesChangedRm++;
-            } else {
-                duplication = false;
-                buildMessage("rm-f", file);
-                break;
-            }
-            result.setDuplicationStatus(duplication);
-        }
-        if (duplication) {
-            result.setMessage(buildMessage("rm-t", files));
-            lastCommitChanges = filesChangedRm;
-            for (String file : files) {
-                storage.remove(file);
-            }
-        }
-
+        result.setMessage(resultMessage);
         return result;
     }
 
     public Result commit(String string) {
+        Result result = new Result();
         if (string == null) {
-            System.out.println("Enter commit message");
-            return null;
-        } else {
-            lastCommitChanges = st.lastChanges();
-            if (result.isSuccessful() && lastCommitChanges != 0) {
-                //TODO try to apply changes
-                commit.setCurrentBranch(branch);
-                commit.setMessage(string);
-                result.setMessage(lastCommitChanges + " files changed");
+                    System.out.println("Enter commit message");
+                    return null;
+                } else {
+                    if (filesChangedAdd == 0){ //TODO - last changes from commit?
+                        resultMessage = "nothing to commit, working tree clean";
+                        result.setMessage(resultMessage);
+                        result.setSuccessful(false);
+                    } else {
+                        resultMessage = filesChangedAdd + " files changed";
+                        result.setMessage(resultMessage);
+                        result.setSuccessful(true);
+                    }
+                }
+        return result;
+      }
+
+    public Result remove(String... files) {
+        boolean existing = true;
+        Result result = new Result();
+        for (String file : files) {
+            if (staging.contains(file)) {
+                continue;
             } else {
-                result.setMessage("nothing to commit, working tree clean");
+                existing = false;
+                resultMessage = file + " did not match any files";
+                result.setMessage(resultMessage);
+                result.setSuccessful(false);
             }
+        }
+        if (existing){
+            int filesChanged = files.length;
+            //TODO - check working or change to foreach?
+            resultMessage = "added " + filesChanged + " for removal.";
+            result.setSuccessful(true);
+            result.setMessage(resultMessage);
         }
         return result;
     }
 
     public void createBranch(String branchName) {
-        // the branch is pointer to a commit
-        if (branch.isExisting()) {
-            result.setMessage("branch " + branchName + "already exists");
-        } else {
-            branch = new Branch(branchName);
-        }
-
+        /*
+         * // the branch is pointer to a commit if (branch.isExisting()) {
+         * result.setMessage("branch " + branchName + "already exists"); } else
+         * { branch = new Branch(branchName); }
+         */
     }
 
     public Result checkoutBranch(String string) {
@@ -113,13 +101,13 @@ public class Repository {
         return null;
     }
 
-    public Branch getBranch() {
-        return branch;
-    }
+    /*
+     * public Branch getBranch() { return branch; }
+     */
 
-    public void setBranch(Branch branch) {
-        this.branch = branch;
-    }
+    /*
+     * public void setBranch(Branch branch) { this.branch = branch; }
+     */
 
     private String buildMessage(String action, String... files) {
         String msg = null;
--- src/bg/sofia/uni/fmi/mjt/git/Result.java
+++ src/bg/sofia/uni/fmi/mjt/git/Result.java
@@ -2,18 +2,19 @@ package bg.sofia.uni.fmi.mjt.git;
 
 public class Result {
     private String message;
+    private boolean successful = false;
     private boolean duplication = false;
+    
     public Result(){
         
     }
     public boolean isSuccessful() {
-         if (duplication) {
-             return false;  
-         } else {
-             return true;
-         } 
+        return successful;
     }
     
+    public void setSuccessful(boolean successful) {
+        this.successful = successful;
+    }
     public String getMessage() {
         return message;
     } 
